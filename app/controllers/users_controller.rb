d8bb8a610f48e01c0d85f63dd3b70f1eed84bcd9
yumkx10 <yuji.i7211@gmail.com>
2023-01-13 23:25:14 +0900

    ユーザー詳細ページへの遷移設定修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,8 @@
 class UsersController < ApplicationController
-  before_action: :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: :show
 
-  def index
-  end
-  
   def show
     @user = User.find(params[:id])
     @prototypes = @user.prototypes
   end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :profile, :occupation, :position).merge(user_id: current_user.id)
-  end
-
 end
